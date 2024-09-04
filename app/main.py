902a2761e09e852710fce5bb4605b3228993beeb
sreejithliterally <imsreejithofficial@gmail.com>
2024-09-05 01:50:38 +0530

    form 21

--- app/main.py
+++ app/main.py
@@ -31,9 +31,40 @@ os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 with open("placement_config.json", "r") as config_file:
     placement_config = json.load(config_file)
+with open("form21_config.json", "r") as config_file:
+    form21_config = json.load(config_file)
+
+@app.post("/process_pdf/form21")
+async def process_pdf(pdf: UploadFile = File(...)):
+    if pdf.content_type != "application/pdf":
+        raise HTTPException(status_code=400, detail="Uploaded file is not a PDF.")
+   
+    # Save uploaded files temporarily
+    pdf_id = str(uuid.uuid4())
+    pdf_path = os.path.join(OUTPUT_DIR, f"{pdf_id}_{pdf.filename}")
+    
+    with open(pdf_path, "wb") as pdf_file:
+        pdf_file.write(await pdf.read())
+    
+
+    signature_path=''
+    finance_company = ''
+    # Process the PDF
+    output_pdf_path = os.path.join(OUTPUT_DIR, f"processed_{pdf_id}_{pdf.filename}")
+    try:
+        add_stamps_and_signature(pdf_path, signature_path, output_pdf_path, form21_config, finance_company)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error processing PDF: {e}")
+    finally:
+        # Clean up temporary files
+        os.remove(pdf_path)
+     
+    
+    return FileResponse(output_pdf_path, filename=f"processed_{pdf.filename}", media_type='application/pdf')
 
 
-@app.post("/process_pdf/")
+
+@app.post("/process_pdf/form20")
 async def process_pdf(pdf: UploadFile = File(...), signature: UploadFile = File(...),finance_company: str = Form(...)):
     if pdf.content_type != "application/pdf":
         raise HTTPException(status_code=400, detail="Uploaded file is not a PDF.")
